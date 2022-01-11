338559cf57c76ab5bc76850dda034340613d39fd
Chris Eppolito <ceppoli1@binghamton.edu>
2022-01-10 19:52:20 -0500

    Add script to create PDF of one chapter.

---
+++ produceChapter.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/sh
+
+# produce only a chapter of the thesis in its own tex file
+
+# copy the sources file
+cp sources.bib tmp/sources.bib
+
+# make a file
+OUTFILE="tmp/$2.tex"
+# make document header
+echo '\\documentclass[12pt]{article}
+\\newcommand{\\chapter}{\\section}
+\\newcommand{\\spChapter}{\\section}
+
+' > $OUTFILE
+# include preamble.tex
+cat "preamble.tex" >> $OUTFILE
+# include bibliography formatting
+cat "format/bibliography.tex" >> $OUTFILE
+# include the sources file and begin document
+echo '\\addbibresource{sources.bib}
+\\begin{document}\n\n
+
+' >> $OUTFILE
+# include the named chapter (do NOT include the .tex)
+cat "content/$1.tex" >> $OUTFILE
+# add bibliography and end document
+echo '
+
+\\printbibliography
+\\end{document}
+' >> $OUTFILE
